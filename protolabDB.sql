41d7be988b0cae3f59b4673ceb227e1c9e4b0fe9
Wojtek <wkozyra@wp.pl>
2018-05-21 17:58:11 +0200

    Update database

--- protolabDB.sql
+++ protolabDB.sql
@@ -1,170 +1,244 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: protolab_db
--- ------------------------------------------------------
--- Server version	5.7.21-log
+-- Host: 127.0.0.1
+-- Czas generowania: 21 Maj 2018, 17:56
+-- Wersja serwera: 10.1.31-MariaDB
+-- Wersja PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `dane_logowania`
+-- Baza danych: `protolabdb`
 --
 
-DROP TABLE IF EXISTS `dane_logowania`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dane_logowania`
+--
+
 CREATE TABLE `dane_logowania` (
-  `ID_konta` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_konta` int(11) NOT NULL,
   `Login` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  `Haslo` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  PRIMARY KEY (`ID_konta`),
-  UNIQUE KEY `Haslo_UNIQUE` (`Haslo`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `Haslo` varchar(45) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `dane_logowania`
+-- Zrzut danych tabeli `dane_logowania`
 --
 
-LOCK TABLES `dane_logowania` WRITE;
-/*!40000 ALTER TABLE `dane_logowania` DISABLE KEYS */;
-INSERT INTO `dane_logowania` VALUES (1,'admin','admin'),(2,'user1','aaa'),(3,'user2','bbb'),(4,'user3','ccc'),(5,'user4','ddd'),(6,'user5','eee'),(7,'user6','fff'),(8,'user7','ggg'),(9,'user8','hhh'),(10,'user9','iii'),(11,'user10','jjj'),(12,'user11','kkk'),(13,'user12','lll'),(14,'user13','mmm'),(15,'user14','nnn');
-/*!40000 ALTER TABLE `dane_logowania` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `dane_logowania` (`ID_konta`, `Login`, `Haslo`) VALUES
+(1, 'admin', 'admin'),
+(2, 'user1', 'aaa'),
+(3, 'user2', 'bbb'),
+(4, 'user3', 'ccc'),
+(5, 'user4', 'ddd'),
+(6, 'user5', 'eee'),
+(7, 'user6', 'fff'),
+(8, 'user7', 'ggg'),
+(9, 'user8', 'hhh'),
+(10, 'user9', 'iii'),
+(11, 'user10', 'jjj'),
+(12, 'user11', 'kkk'),
+(13, 'user12', 'lll'),
+(14, 'user13', 'mmm'),
+(15, 'user14', 'nnn');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `przedmioty`
+-- Struktura tabeli dla tabeli `przedmioty`
 --
 
-DROP TABLE IF EXISTS `przedmioty`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `przedmioty` (
-  `ID_przedmiotu` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_przedmiotu` int(11) NOT NULL,
   `Nazwa` varchar(45) COLLATE utf8_polish_ci NOT NULL,
   `Rodzaj` varchar(45) COLLATE utf8_polish_ci DEFAULT NULL,
   `Ilosc` int(11) NOT NULL,
-  `Status` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  PRIMARY KEY (`ID_przedmiotu`)
+  `Status` varchar(45) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `przedmioty`
+-- Zrzut danych tabeli `przedmioty`
 --
 
-LOCK TABLES `przedmioty` WRITE;
-/*!40000 ALTER TABLE `przedmioty` DISABLE KEYS */;
-/*!40000 ALTER TABLE `przedmioty` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `przedmioty` (`ID_przedmiotu`, `Nazwa`, `Rodzaj`, `Ilosc`, `Status`) VALUES
+(1, 'monitor', 'sprzęt komputerowy', 40, 'w magazynie'),
+(2, 'klawiatura', 'sprzęt komputerowy', 30, 'w magazynie'),
+(3, 'mysz komputerowa', 'sprzęt komputerowy', 15, 'w magazynie'),
+(4, 'głośniki', 'sprzęt komputerowy', 25, 'w magazynie'),
+(5, 'drukarka', 'sprz?t komputerowy', 20, 'w magazynie'),
+(6, 'skaner', 'sprz?t komputerowy', 35, 'w magazynie'),
+(7, 'fax', 'sprzet komputerowy', 50, 'w magazynie');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `rezerwacje`
+-- Struktura tabeli dla tabeli `rezerwacje`
 --
 
-DROP TABLE IF EXISTS `rezerwacje`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rezerwacje` (
-  `idRezerwacji` int(11) NOT NULL AUTO_INCREMENT,
+  `idRezerwacji` int(11) NOT NULL,
   `ID_uzytkownika` int(11) NOT NULL,
   `ID_przedmiotu` int(11) NOT NULL,
   `od_kiedy` date NOT NULL,
   `do_kiedy` date NOT NULL,
-  `nazwaPrzedmiotu` varchar(45) COLLATE utf8_polish_ci NOT NULL,
-  `ilosc` int(11) NOT NULL,
-  PRIMARY KEY (`idRezerwacji`),
-  UNIQUE KEY `idRezerwacji_UNIQUE` (`idRezerwacji`),
-  KEY `ID_uzytkownika_idx` (`ID_uzytkownika`),
-  KEY `ID_przedmiotu_idx` (`ID_przedmiotu`),
-  CONSTRAINT `ID_przedmiotu` FOREIGN KEY (`ID_przedmiotu`) REFERENCES `przedmioty` (`ID_przedmiotu`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `ID_uzytkownika` FOREIGN KEY (`ID_uzytkownika`) REFERENCES `uzytkownicy` (`ID_uzytkownika`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `ilosc` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `rezerwacje`
+-- Zrzut danych tabeli `rezerwacje`
 --
 
-LOCK TABLES `rezerwacje` WRITE;
-/*!40000 ALTER TABLE `rezerwacje` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rezerwacje` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `rezerwacje` (`idRezerwacji`, `ID_uzytkownika`, `ID_przedmiotu`, `od_kiedy`, `do_kiedy`, `ilosc`) VALUES
+(2, 3, 7, '2018-05-22', '2018-05-25', 10);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `uprawnienia`
+-- Struktura tabeli dla tabeli `uprawnienia`
 --
 
-DROP TABLE IF EXISTS `uprawnienia`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `uprawnienia` (
   `ID_uprawnienia` int(11) NOT NULL,
-  `rodzajUprawnienia` varchar(45) COLLATE utf8mb4_polish_ci NOT NULL,
-  PRIMARY KEY (`ID_uprawnienia`),
-  UNIQUE KEY `rodzajUprawnienia_UNIQUE` (`rodzajUprawnienia`)
+  `rodzajUprawnienia` varchar(45) COLLATE utf8mb4_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `uprawnienia`
+-- Zrzut danych tabeli `uprawnienia`
 --
 
-LOCK TABLES `uprawnienia` WRITE;
-/*!40000 ALTER TABLE `uprawnienia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `uprawnienia` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `uprawnienia` (`ID_uprawnienia`, `rodzajUprawnienia`) VALUES
+(2, 'admin'),
+(1, 'student'),
+(3, 'szef kola');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `uzytkownicy`
+-- Struktura tabeli dla tabeli `uzytkownicy`
 --
 
-DROP TABLE IF EXISTS `uzytkownicy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `uzytkownicy` (
-  `ID_uzytkownika` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_uzytkownika` int(11) NOT NULL,
   `ID_uprawnienia` int(11) NOT NULL,
   `imie` varchar(45) COLLATE utf8mb4_polish_ci NOT NULL,
   `nazwisko` varchar(45) COLLATE utf8mb4_polish_ci NOT NULL,
   `numerTel` int(11) NOT NULL,
   `email` varchar(45) COLLATE utf8mb4_polish_ci NOT NULL,
-  `pesel` int(11) NOT NULL,
-  PRIMARY KEY (`ID_uzytkownika`),
-  UNIQUE KEY `pesel_UNIQUE` (`pesel`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `numerTel_UNIQUE` (`numerTel`),
-  UNIQUE KEY `ID_uprawnienia_UNIQUE` (`ID_uprawnienia`),
-  UNIQUE KEY `ID_uzytkownika_UNIQUE` (`ID_uzytkownika`),
-  CONSTRAINT `ID_uprawnienia` FOREIGN KEY (`ID_uprawnienia`) REFERENCES `uprawnienia` (`ID_uprawnienia`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `pesel` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `uzytkownicy`
+-- Zrzut danych tabeli `uzytkownicy`
 --
 
-LOCK TABLES `uzytkownicy` WRITE;
-/*!40000 ALTER TABLE `uzytkownicy` DISABLE KEYS */;
-/*!40000 ALTER TABLE `uzytkownicy` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `uzytkownicy` (`ID_uzytkownika`, `ID_uprawnienia`, `imie`, `nazwisko`, `numerTel`, `email`, `pesel`) VALUES
+(2, 2, 'Jan', 'Kowalski', 874136899, 'kowalski@gmail.com', 45213654874),
+(3, 3, 'Anna', 'Woźniak', 789654123, 'awozniak@op.pl', 74533216549),
+(4, 1, 'Marek', 'Nowak', 745861234, 'aads@op.pl', 59234851346);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `dane_logowania`
+--
+ALTER TABLE `dane_logowania`
+  ADD PRIMARY KEY (`ID_konta`),
+  ADD UNIQUE KEY `Haslo_UNIQUE` (`Haslo`);
+
+--
+-- Indeksy dla tabeli `przedmioty`
+--
+ALTER TABLE `przedmioty`
+  ADD PRIMARY KEY (`ID_przedmiotu`);
+
+--
+-- Indeksy dla tabeli `rezerwacje`
+--
+ALTER TABLE `rezerwacje`
+  ADD PRIMARY KEY (`idRezerwacji`),
+  ADD UNIQUE KEY `idRezerwacji_UNIQUE` (`idRezerwacji`),
+  ADD KEY `ID_uzytkownika_idx` (`ID_uzytkownika`),
+  ADD KEY `ID_przedmiotu_idx` (`ID_przedmiotu`);
+
+--
+-- Indeksy dla tabeli `uprawnienia`
+--
+ALTER TABLE `uprawnienia`
+  ADD PRIMARY KEY (`ID_uprawnienia`),
+  ADD UNIQUE KEY `rodzajUprawnienia_UNIQUE` (`rodzajUprawnienia`);
+
+--
+-- Indeksy dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`ID_uzytkownika`),
+  ADD UNIQUE KEY `pesel_UNIQUE` (`pesel`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD UNIQUE KEY `numerTel_UNIQUE` (`numerTel`),
+  ADD UNIQUE KEY `ID_uprawnienia_UNIQUE` (`ID_uprawnienia`),
+  ADD UNIQUE KEY `ID_uzytkownika_UNIQUE` (`ID_uzytkownika`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `dane_logowania`
+--
+ALTER TABLE `dane_logowania`
+  MODIFY `ID_konta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT dla tabeli `przedmioty`
+--
+ALTER TABLE `przedmioty`
+  MODIFY `ID_przedmiotu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT dla tabeli `rezerwacje`
+--
+ALTER TABLE `rezerwacje`
+  MODIFY `idRezerwacji` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `ID_uzytkownika` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `rezerwacje`
+--
+ALTER TABLE `rezerwacje`
+  ADD CONSTRAINT `ID_przedmiotu` FOREIGN KEY (`ID_przedmiotu`) REFERENCES `przedmioty` (`ID_przedmiotu`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ID_uzytkownika` FOREIGN KEY (`ID_uzytkownika`) REFERENCES `uzytkownicy` (`ID_uzytkownika`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ograniczenia dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD CONSTRAINT `ID_uprawnienia` FOREIGN KEY (`ID_uprawnienia`) REFERENCES `uprawnienia` (`ID_uprawnienia`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-04-15 15:48:40
