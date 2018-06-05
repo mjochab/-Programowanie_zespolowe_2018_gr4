3c60e3a9fdb9b7e7e39b2da695c5affc46364c9f
DominikMaga <dominik-maga@wp.pl>
2018-06-05 10:14:58 +0200

    validation add Item

---
+++ ProtoLab/src/protolab/exceptions/TypeItemNotSelectedException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package protolab.exceptions;
+
+/**
+ *
+ * @author domin
+ */
+public class TypeItemNotSelectedException extends Exception{
+            public TypeItemNotSelectedException(String s) {
+         super(s);  
+}
+}
